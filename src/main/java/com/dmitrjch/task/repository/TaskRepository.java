d320e2faf5124506095e2c4726df7a2e70434cca
dmitrij <dmitrij.chvanov@gmail.com>
2023-12-08 15:11:54 +0400

    added repository

---
+++ src/main/java/com/dmitrjch/task/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.dmitrjch.task.repository;
+
+import com.dmitrjch.task.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+}
