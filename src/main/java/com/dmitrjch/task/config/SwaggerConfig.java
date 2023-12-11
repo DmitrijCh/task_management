766e9b43dd122094695c79f6c8b4e47beb895619
dmitrij <dmitrij.chvanov@gmail.com>
2023-12-11 15:47:15 +0400

    added Swagger configurations

---
+++ src/main/java/com/dmitrjch/task/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.dmitrjch.task.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(new Info().title("Tasks API").version("0.0.1"))
+                .components(new Components()
+                        .addSecuritySchemes("JWT", new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")))
+                .addSecurityItem(new SecurityRequirement().addList("JWT"));
+    }
+}
